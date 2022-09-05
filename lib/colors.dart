7ccce5c97238c1de765c3fe57d68a63a7836492a
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-05 17:28:25 +0300

    color change

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,6 +5,3 @@ var yaziRenk1 = const Color(0xffa9a5a5);
 var yaziRenk2 = const Color(0xFFeaeef2);
 var yaziRenk3 =const Color(0xFF202124);
 var arkaPlan = const Color(0xffeeeef1);
-//fa0050 1.
-//f72858 2.
-//fa0050 3.
