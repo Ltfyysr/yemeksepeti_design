821faff37528950bf058bfca56b6a8cd4b1038d7
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-06 16:08:45 +0300

    drawer page added

--- lib/drawer_sayfa.dart
+++ lib/drawer_sayfa.dart
@@ -10,23 +10,24 @@ class DrawerSayfa extends StatefulWidget {
 class _DrawerSayfaState extends State<DrawerSayfa> {
   @override
   Widget build(BuildContext context) {
-    return  Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: [
-        DrawerHeader(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Giriş Yap/Hesap Oluştur",
-              style: TextStyle(color: Colors.white, fontSize: 14),
-            ),
-          ],
-        ),
-        decoration: BoxDecoration(color: Color(0xFFf72858)),
-      ),
-   ])
+    return Drawer(
+        child: ListView(padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Giriş Yap/Hesap Oluştur",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                ),
+                decoration: const BoxDecoration(color: Color(0xFFf72858)),
+              ),
+            ]
+        )
     );
-
   }
 }
